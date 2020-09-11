d5821903bcae3001e5bc69760ad629757d233a51
ajtalla <ajaykumar2797@gmail.com>
2020-09-11 09:50:33 -0400

    new program

--- src/main/java/com/myorganization/app/Main.java
+++ src/main/java/com/myorganization/app/Main.java
@@ -1,8 +1,18 @@
-package com.myorganization.app;
+import java.util.Scanner;
+public class AddTwoNumbers2 {
 
-public class Main {
-	public static void main(String[] args) {
-
-		System.out.println("Hello, world!");
-	}
+    public static void main(String[] args) {
+        
+        int num1, num2, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        num1 = sc.nextInt();
+        
+        System.out.println("Enter Second Number: ");
+        num2 = sc.nextInt();
+        
+        sc.close();
+        sum = num1 + num2;
+        System.out.println("Sum of these numbers: "+sum);
+    }
 }
